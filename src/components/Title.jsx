9f3def0a1c79c5a8bc416585c3fee5f0afa1c4ea
Andraž Kos <andraz.kos@gmail.com>
2024-05-13 09:34:39 +0000

    refactor: Update handle color and style in Title component
    The handle color and style in the Title component have been updated to use the classNames ['text-lg', 'font-thin', 'text-zinc-400', 'leading-none'] instead of ['text-xl', 'font-thin', 'text-gray-200', 'leading-none']. This change improves consistency and enhances the visual representation of the title component.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,20 @@
+/**
+ * Renders a title component with optional classNames, children, and id.
+ * @param {Object} props - The component props.
+ * @param {string[]} [props.classNames=['text-lg', 'font-thin', 'text-zinc-400', 'leading-none']] - The optional classNames for the title component.
+ * @param {ReactNode} props.children - The children to be rendered within the title component.
+ * @param {string} [props.id] - The optional id for the title component.
+ * @returns {JSX.Element} The rendered title component.
+ */
 const Title = ({
-  classNames = ['text-xl', 'font-thin', 'text-gray-200', 'leading-none'],
+  classNames = ['text-lg', 'font-thin', 'text-zinc-400', 'leading-none'],
   children,
   id,
 }) => {
   return (
-    <h1 className={classNames.join(' ')}>
+    <div className={classNames.join(' ')}>
       {children} <sup>[{id}]</sup>
-    </h1>
+    </div>
   )
 }
 
