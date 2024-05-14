633fd4cebaf0e72d4653b55d7996afe09a03f54a
Andraž Kos <andraz.kos@gmail.com>
2024-05-14 03:37:53 +0000

    refactor: Update useStoreExtension hook to improve store management
    This refactor updates the useStoreExtension hook to enhance the management of the store's state. It adds functionality for adding items to the store, retrieving items by their ID, and initializing the store with initial items. This improves code organization, reusability, and overall store management.

--- src/hooks/useStoreExtension.jsx
+++ src/hooks/useStoreExtension.jsx
@@ -7,7 +7,7 @@ import { useStoreApi } from '@xyflow/react'
  * @param {Object} options - The options for the store extension.
  * @param {Array} options.initialItems - The initial items to add to the store.
  */
-const useStoreExtension = ({ initialItems }) => {
+const useStoreExtension = ({ initialItems = [] }) => {
   // Get the handle to the store api
   const store = useStoreApi()
 
@@ -24,7 +24,6 @@ const useStoreExtension = ({ initialItems }) => {
       // If the store is already extended, abort processing
       if (isExtended) return prevState
 
-      // Extend the store
       return {
         // Keep the previous state
         ...prevState,
@@ -36,43 +35,13 @@ const useStoreExtension = ({ initialItems }) => {
         itemLookup: new Map(),
 
         // Add an item to the store
-        addItem: item => {
-          // If item does not have an id
-          if (!item.id) {
-            // Check if there are no items in the store with the same content
-            const existingItem = store.getState().items.find(existingItem => {
-              // Stringify skipping the id
-              const existingItemString = JSON.stringify(
-                existingItem,
-                (key, value) => (key === 'id' ? undefined : value)
-              )
-              const itemString = JSON.stringify(item)
-              return existingItemString === itemString
-            })
-
-            // If there is an item with the same values, abort
-            if (existingItem) return
-
-            // Generate a random id
-            item.id = window?.crypto?.randomUUID()?.slice(-4) || Math.random()
-          }
-
-          // If item is already in the store, do not add it again
-          if (store.getState().itemLookup?.has(String(item.id))) return
-
-          // Save changes to the store
-          store.setState(prevState => ({
-            ...prevState,
-            items: [...prevState.items, item],
-            itemLookup: new Map([
-              ...prevState.itemLookup,
-              [String(item.id), item],
-            ]),
-          }))
-        },
+        addItem: item => addItemToStore(store, item),
 
         // Get an item by its id
-        getItem: id => store.getState().itemLookup.get(String(id)),
+        getItem: id => getItemFromStore(store, id),
+
+        // Generate an unique ID
+        generateId,
       }
     })
 
@@ -89,8 +58,94 @@ const useStoreExtension = ({ initialItems }) => {
     }
 
     // Add the initial items to the store
-    initialItems.forEach(item => store.getState().addItem(item))
+    addInitialItemsToStore(store, initialItems)
   }, [store, initialItems])
+}
+
+/**
+ * Generates an unique ID, default length of 6 is highly probable to be unique for over 10_000 generated IDs.
+ * @param {number} [length=6] - Optional length of the generated ID.
+ * @returns {string} The generated ID.
+ */
+
+const generateId = (length = 6) => {
+  const randomArray = new Uint8Array(Math.ceil((length * Math.log2(34)) / 8))
+  window.crypto.getRandomValues(randomArray)
+  const id = Array.from(randomArray)
+    .reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '')
+    .toString(36)
+    .toUpperCase()
+    .replaceAll('O', '0')
+    .replaceAll('I', '1')
+    .slice(-length)
+  return id
+}
+
+/**
+ * Retrieves an item from the store based on the provided ID.
+ *
+ * @param {Object} store - The store object.
+ * @param {number} id - The ID of the item to retrieve.
+ * @returns {any} The item from the store.
+ */
+const getItemFromStore = (store, id) =>
+  store.getState().itemLookup.get(String(id))
+
+/**
+ * Adds an item to the store.
+ *
+ * @param {Object} store - The store object.
+ * @param {Object} item - The item to add to the store.
+ */
+const addItemToStore = (store, item) => {
+  // Get a handle to the store state
+  const state = store.getState()
+
+  // If item does not have an id
+  if (!item.id) {
+    // Check if there are no items in the store with the same content
+    const existingItem = state.items.find(existingItem => {
+      // Stringify items without the id property
+      const existingItemString = JSON.stringify(existingItem, (key, value) =>
+        key === 'id' ? undefined : value
+      )
+      // Compare the stringified content
+      return existingItemString === JSON.stringify(item)
+    })
+
+    // If there is an item with the same values, skip adding it to the store
+    if (existingItem) return
+
+    // Generate a random id for the item without an id
+    item.id = state.generateId()
+  }
+
+  // If the item is already in the store, do not add it again
+  if (state.getItem(item.id)) return
+
+  // Save changes to the store
+  store.setState(prevState => ({
+    ...prevState,
+    items: [...prevState.items, item],
+    itemLookup: new Map([...prevState.itemLookup, [String(item.id), item]]),
+  }))
+}
+
+/**
+ * Adds initial items to the store.
+ *
+ * @param {Object} store - The store object.
+ * @param {Array} initialItems - The array of initial items to be added.
+ */
+const addInitialItemsToStore = (store, initialItems) => {
+  // Sanity check
+  if (!initialItems || !Array.isArray(initialItems)) return
+
+  // Get a handle to the add item function
+  const addItem = store.getState().addItem
+
+  // Loop through the initial items and add them to the store
+  initialItems.forEach(item => addItem(item))
 }
 
 export default useStoreExtension
