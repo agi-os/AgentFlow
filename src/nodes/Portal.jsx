40746658e49dde1f0d0c4d4b5c0fa0699cc245fd
Andraž Kos <andraz.kos@gmail.com>
2024-05-15 04:25:36 +0000

    Refactor Portal component to improve code organization and readability

--- src/nodes/Portal.jsx
+++ src/nodes/Portal.jsx
@@ -1,52 +1,63 @@
-/**
- * Portal node is a pair of nodes that are connected by a portal, instantly transporting items between them.
- * Any item ingested by the Input Portal will disappear and reappear at the Output Portal.
- *
- */
-
 import Title from '../components/Title'
 import SignalHandles from '../signals/handle'
 import { BeltSource, BeltTarget } from '../components/BeltPort'
 import Semaphore from '../components/Semaphore'
 
-export const InputPortal = ({ id, selected }) => {
+/**
+ * Portal node is a pair of nodes that are connected by a portal, instantly transporting items between them.
+ * Any item ingested by the Input Portal will disappear and reappear at the Output Portal.
+ * @param {object} props - The properties of the portal.
+ * @param {string} props.id - The ID of the portal.
+ * @param {boolean} props.selected - Indicates whether the portal is selected.
+ * @param {ReactNode} props.children - The child elements of the portal.
+ * @returns {JSX.Element} The rendered portal component.
+ */
+const Portal = ({ id, selected, children }) => {
   // Prepare the class names based on the selected state
   const selectedClassNames = selected
     ? ['outline-orange-700']
     : ['outline-transparent']
 
+  // Combine the base and selected class names
   const classNames = [...baseClassNames, ...selectedClassNames]
 
   // Render the portal
   return (
     <div className={classNames.join(' ')}>
-      <BeltTarget />
       <Title id={id}>🔗 Portal</Title>
-      <div className="flex justify-center">⤴️ abc-def</div>
-      <Semaphore />
       <SignalHandles />
+      <Semaphore />
+      {children}
     </div>
   )
 }
 
-export const OutputPortal = ({ id, selected }) => {
-  // Prepare the class names based on the selected state
-  const selectedClassNames = selected
-    ? ['outline-orange-700']
-    : ['outline-transparent']
+/**
+ * Renders an input portal component.
+ * @param {Object} props - The component props.
+ * @param {string} props.id - The ID of the portal.
+ * @param {boolean} props.selected - Indicates whether the portal is selected.
+ * @returns {JSX.Element} The rendered InputPortal component.
+ */
+export const InputPortal = ({ id, selected }) => (
+  <Portal id={id} selected={selected}>
+    <BeltTarget />
+    <div className="flex justify-center">⤴️ abc-def</div>
+  </Portal>
+)
 
-  const classNames = [...baseClassNames, ...selectedClassNames]
-
-  return (
-    <div className={classNames.join(' ')}>
-      <Title id={id}>🔗 Portal</Title>
-      <div className="flex justify-center">⤵️ abc-def</div>
-      <SignalHandles />
-      <Semaphore />
-      <BeltSource />
-    </div>
-  )
-}
+/**
+ * Renders an output portal component.
+ * @param {string} id - The ID of the portal.
+ * @param {boolean} selected - Indicates whether the portal is selected.
+ * @returns {JSX.Element} The output portal component.
+ */
+export const OutputPortal = ({ id, selected }) => (
+  <Portal id={id} selected={selected}>
+    <div className="flex justify-center">⤵️ abc-def</div>
+    <BeltSource />
+  </Portal>
+)
 
 const baseClassNames = [
   'p-1',
