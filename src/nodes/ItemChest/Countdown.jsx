8d3469dc82ac59e5f3435f9c34ad0807f87cff4a
Andraž Kos <andraz.kos@gmail.com>
2024-05-18 13:43:00 +0000

    refactor: Make semaphore affect the countdown timer and block output when red

--- src/nodes/ItemChest/Countdown.jsx
+++ src/nodes/ItemChest/Countdown.jsx
@@ -5,8 +5,16 @@ import { useStore, useNodeId } from '@xyflow/react'
 import putOnBelt from './putOnBelt'
 
 const Countdown = () => {
-  const { to, count } = useJitteryCountdown({ timer: 2_000 })
+  // Get the node id
   const nodeId = useNodeId()
+
+  // Get the edge semaphore data
+  const semaphore = useStore(s => s.getNode(nodeId)?.data?.semaphore)
+
+  // Get the countdown data
+  const { to, count } = useJitteryCountdown({
+    timer: semaphore === 'green' ? 1500 : semaphore === 'yellow' ? 7500 : 4500,
+  })
 
   // Get store data
   const setItem = useStore(s => s.setItem)
@@ -15,13 +23,16 @@ const Countdown = () => {
 
   useEffect(() => {
     if (count > 0) return
+
+    if (semaphore === 'red') return
+
     // Add item on belt when countdown reaches 0
     putOnBelt({ getLocationItemsSorted, setItem, edges, nodeId })
-  }, [edges, getLocationItemsSorted, nodeId, setItem, count])
+  }, [edges, getLocationItemsSorted, nodeId, setItem, count, semaphore])
 
   return (
     <div className={countdownClassNames.join(' ')}>
-      <Flip to={to} />
+      {semaphore !== 'red' && <Flip to={to} />}
     </div>
   )
 }
