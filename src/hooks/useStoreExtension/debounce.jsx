8e2fa33926b702152a8198bd2bb6e83f01d74b2d
Andraž Kos <andraz.kos@gmail.com>
2024-05-18 10:01:30 +0000

    feat: Extend store with tick functionality
    This commit extends the store with the tick functionality, which includes a tick counter, a tick function, and a tick loop. It also adds the belt drive functionality, which moves items on the belt one step per tick. The speed of the belt drive can be adjusted, and the items on the belts are updated accordingly. This enhancement improves the overall functionality and performance of the application.

--- src/hooks/useStoreExtension/debounce.jsx
+++ src/hooks/useStoreExtension/debounce.jsx
@@ -3,10 +3,10 @@
  * since the last time it was invoked.
  *
  * @param {Function} func - The function to be debounced.
- * @param {number} [wait=50] - The delay in milliseconds.
+ * @param {number} [wait=10] - The delay in milliseconds.
  * @returns {Function} - The debounced function.
  */
-const debounce = (func, wait = 50) => {
+const debounce = (func, wait = 10) => {
   let timeout
   return (...args) => {
     const later = () => {
--- src/hooks/useStoreExtension/index.jsx
+++ src/hooks/useStoreExtension/index.jsx
@@ -2,13 +2,13 @@ import { useEffect } from 'react'
 
 import generateId from './generateId'
 import addInitialItemsToStore from './addInitialItemsToStore'
-import { useStoreApi } from '@xyflow/react'
+import { useStore, useStoreApi } from '@xyflow/react'
 import debounce from './debounce'
 
 import addItem from './addItem'
 import updateItemLocationLookup from './updateItemLocationLookup'
 import updateItemLookup from './updateItemLookup'
-import getLocationItems from './getLocationItems'
+import _getLocationItems from './getLocationItems'
 import getLocationItemsSorted from './getLocationItemsSorted'
 import lookup from './lookup'
 
@@ -21,7 +21,11 @@ import lookup from './lookup'
 const useEnhancedStore = ({ initialItems }) => {
   const store = useStoreApi()
 
+  // Extend the store with custom methods
   useEffect(() => {
+    // Sanity check
+    if (!store) return
+
     // If the store has been extended previously, abort
     if (store.getState().generateId) return
 
@@ -29,17 +33,27 @@ const useEnhancedStore = ({ initialItems }) => {
     store.setState(draft => ({
       ...draft,
 
-      // System wide speed of all belts in pixels per second
-      speed: 37,
+      // Tick length in ms, this is base unit of all operations, 16.666ms = 60fps
+      tickLength: 50,
+
+      // System wide speed of all belts
+      speed: 1000,
+
       setSpeed: debounce(speed =>
-        store.setState(
-          draft => ({
+        store.setState(draft => {
+          console.log('Setting speed from', draft.speed, 'to', speed)
+          return {
             ...draft,
             speed: parseFloat(speed),
-          }),
-          1000
-        )
+          }
+        }, 200)
       ),
+
+      // List of all belts
+      beltIds: [],
+
+      // Id of the setInterval loop calling beltDriveTick
+      beltDriveIntervalId: null,
 
       items: [],
       addItem: item => addItem({ store, item }),
@@ -60,7 +74,7 @@ const useEnhancedStore = ({ initialItems }) => {
 
       itemLocationLookup: new Map(),
       updateItemLocationLookup: debounce(() => updateItemLocationLookup(store)),
-      getLocationItems: locationId => getLocationItems({ store, locationId }),
+      getLocationItems: locationId => _getLocationItems({ store, locationId }),
       getLocationItemsSorted: locationId =>
         getLocationItemsSorted({ store, locationId }),
 
@@ -78,6 +92,136 @@ const useEnhancedStore = ({ initialItems }) => {
           ),
     }))
   }, [store])
+
+  // Get the tick counter from the store
+  const tickCounter = useStore(s => s.tickCounter)
+
+  // Tick counter will increment every tick, tasks that are not time critical can be run on every nth tick
+  useEffect(() => {
+    if (tickCounter % 50 !== 0) return
+
+    console.log('Tick counter', tickCounter)
+
+    // Get the current edges from the store
+    const draft = store.getState()
+
+    // Generate the list of belt ids
+    const newBeltIds = draft.edges
+      .filter(edge => edge.type === 'queue')
+      .map(edge => edge.id)
+
+    // Compare the new list of belt ids with the old list of belt ids
+    const hasChanged = newBeltIds.some(
+      (id, index) => id !== draft.beltIds[index]
+    )
+
+    // If the list of belt ids has changed, update the store
+    if (hasChanged) {
+      store.setState(draft => ({
+        ...draft,
+        beltIds: newBeltIds,
+      }))
+    }
+  }, [tickCounter, store])
+
+  // Get the tick length and tick function from the store
+  const tickLength = useStore(s => s.tickLength)
+  const tick = useStore(s => s.tick)
+
+  // Start the tick loop
+  useEffect(() => {
+    // Sanity check
+    if (!tick || !tickLength) return
+    const tickLoop = setInterval(tick, tickLength)
+    return () => clearInterval(tickLoop)
+  }, [tick, tickLength])
+
+  // Get the belt drive function from the store
+  const beltDrive = useStore(s => s.beltDrive)
+
+  // Extend the store with the tick functionality
+  useEffect(() => {
+    // Sanity check
+    if (!store || !beltDrive) return
+
+    // Extend the store with the tick count if it does not exist
+    store.setState(draft => {
+      return {
+        ...draft,
+        tickCounter: 0,
+      }
+    })
+
+    // Extend the store with the tick function
+    store.setState(draft => {
+      return {
+        ...draft,
+        tick: () => {
+          // Increment the tick counter
+          store.setState(draft => {
+            return {
+              ...draft,
+              tickCounter: draft.tickCounter + 1,
+            }
+          })
+
+          // Call the belt drive function
+          beltDrive()
+        },
+      }
+    })
+  }, [store, beltDrive])
+
+  // Get the speed, belt ids and getLocationItems function from the store
+  const speed = useStore(s => s.speed)
+  const beltIds = useStore(s => s.beltIds)
+  const getLocationItems = useStore(s => s.getLocationItems)
+
+  // Extend the store with the belt drive functionality
+  useEffect(() => {
+    // Sanity check
+    if (!speed) return
+    if (!getLocationItems) return
+
+    console.log('Updating belt drive store', {
+      store,
+      speed,
+      getLocationItems,
+      beltIds,
+    })
+
+    store.setState(draft => {
+      return {
+        ...draft,
+        // Belt drive called every tick to move items on the belt one step
+        beltDrive: () => {
+          // Get the distance change per tick
+          const delta = speed / 100000
+
+          // Get all items on all belts in flat array
+          const items = beltIds.reduce((acc, id) => {
+            const items = getLocationItems(id)
+            return acc.concat(items)
+          }, [])
+
+          // Get all items with a distance more than 0
+          const itemsToMove = items.filter(item => item?.location?.distance > 0)
+
+          // Move all items on all belts which can be moved
+          itemsToMove.forEach(item => {
+            // Update the item's location
+            store.getState().setItem({
+              ...item,
+              location: {
+                ...item?.location,
+                distance: item?.location?.distance - delta,
+              },
+            })
+          })
+        },
+      }
+    })
+  }, [store, speed, getLocationItems, beltIds])
 
   // Initializing with initial items
   useEffect(() => {
