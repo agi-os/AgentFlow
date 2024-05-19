b5393e5d23df5dae62c5af65425139523d92453b
Andraž Kos <andraz.kos@gmail.com>
2024-05-19 02:52:09 +0000

    refactor: Improve debounce function in useStoreExtension
    Refactor the debounce function in the useStoreExtension hook to enhance its flexibility and readability. Update the function signature to accept either a function or a string as the first argument. If a string is provided, the corresponding function will be used for debouncing. This change improves the usability of the debounce function and makes it more intuitive for developers to use.

--- src/hooks/useStoreExtension/debounce.jsx
+++ src/hooks/useStoreExtension/debounce.jsx
@@ -2,15 +2,31 @@
  * Debounces a function, ensuring it is only called after a specified delay
  * since the last time it was invoked.
  *
- * @param {Function} func - The function to be debounced.
+ * @param {Function|string} funcOrName - The function to be debounced or the name of the function.
+ * @param {Function} [func] - The function to be debounced if the first argument is a name.
  * @param {number} [wait=10] - The delay in milliseconds.
  * @returns {Function} - The debounced function.
  */
-const debounce = (func, wait = 10) => {
+const debounce = (...args) => {
   let timeout
+  let name
+  let func
+  let wait = 100
+
+  if (typeof args[0] === 'string') {
+    name = args[0]
+    func = args[1]
+    wait = args[2] || wait
+  } else {
+    func = args[0]
+    wait = args[1] || wait
+    name = func.name || 'anonymous'
+  }
+
   return (...args) => {
     const later = () => {
       clearTimeout(timeout)
+      // console.log('calling debounced function', { name, func, args })
       func(...args)
     }
     clearTimeout(timeout)
